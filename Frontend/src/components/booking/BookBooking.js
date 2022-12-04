f6e1ceaf2e0236f10a54bcfa74f377073d77b6b8
Devansh Bansal <devanshd0@gmail.com>
2022-12-03 19:44:44 -0800

    Adding timepicker for drone booking

--- Frontend/src/components/booking/BookBooking.js
+++ Frontend/src/components/booking/BookBooking.js
@@ -31,7 +31,7 @@ function Copyright() {
   );
 }
 
-const steps = ['Select the area', 'Select the drone', 'Confirm the booking'];
+const steps = ['Select the area, date and time', 'Select the drone', 'Confirm the booking'];
 
 
 
