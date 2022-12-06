ad581a22cf021a23815c2c5e193bf4a5f828ceef
Spartan <nimish.lingesh@gmail.com>
2022-12-05 16:41:35 -0800

    Upload drone image

--- Frontend/src/components/drone/DroneDetails.js
+++ Frontend/src/components/drone/DroneDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -7,6 +7,8 @@ import Checkbox from '@mui/material/Checkbox';
 import { Card } from '@mui/material';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, message, Upload } from 'antd';
 
 const DroneDetails = props => {
 
@@ -19,6 +21,53 @@ const DroneDetails = props => {
     })
   }
 
+  const props_upload = {
+    name: 'file',
+    listType: 'picture',
+    beforeUpload: (file) => {
+      return false;
+    },
+    // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    // headers: {
+    //   authorization: 'authorization-text',
+    // },
+    onChange(info) {
+      console.log("info file - " + info.file);
+      console.log("info fileList - " + info.fileList);
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+
+  // const [image, setImage] = useState('');
+
+  // function handleImage(e){
+  //   console.log(e.target.files);
+  //   setImage(e.target.files[0]);
+  // }
+  // function handleApi(){
+  //   console.log(image);
+  //   // setFile(URL.createObjectURL(image))
+  //   const file = image;
+  //   const reader = new FileReader();
+  //   reader.onloadend = () => {
+  //     // convert file to base64 String
+  //     const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+  //     // store file
+  //     localStorage.setItem('wallpaper', base64String);
+  //     // display image
+  //     document.body.style.background = `url(data:image/png;base64,${base64String})`;
+  //   };
+  //   reader.readAsDataURL(file);
+    
+  // }
 
   return (
     <React.Fragment>
@@ -93,6 +142,17 @@ const DroneDetails = props => {
             <MenuItem value={'Phantom'}>DJI Phantom Pro 4 Surveillence</MenuItem>
             <MenuItem value={'Agras'}>DJI Agras T20 Payload</MenuItem>
             </Select>
+          </Grid>
+        <Grid item xs={12}>
+          <Upload.Dragger {...props_upload}>
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload.Dragger>
+          {/* <input type="file" name="file" onChange={handleImage}/>
+          <button onClick={handleApi}>Upload</button> */}
+          </Grid>
+        <Grid item xs={12}>
+          <br></br>
+          <br></br>
           </Grid>
         {/* <Grid item xs={12}>
           <FormControlLabel
